16b80330298c51ac1df6a1302a6db5642db04900
Dan Moseley <danomoseley@gmail.com>
2023-09-14 13:12:20 -0400

    Setup initial framework

---
+++ src/routes/+page.svelte
@@ -0,0 +1,69 @@
+<div class="lodging-info">
+  <p>
+    Lodging in Cooperstown is already quickly filling up for 2024. If you are planning to celebrate with us and need a place to stay during your visit, we urge you to book your accommodations now!
+  </p>
+
+  <p>
+    There is a price point for everyone in Cooperstown, but the earlier you book the better! As you may or may not know, Cooperstown is home to the Baseball Hall of Fame and Cooperstown Dreams Park which hosts over 100 little league teams, and their families, from all over the country each and every week during the summer months. This means our out of towners and the “baseball people” are in competition for the best places to stay.
+  </p>
+
+  <p>
+    For the true Cooperstown experience we would recommend staying on Otsego Lake or in the Village of Cooperstown at one of the various accommodations.
+  </p>
+
+  <h2>Cooperstown Hotels:</h2>
+
+  <ul>
+    <li>
+      <a target="_blank" rel="noreferrer" href="https://www.otesaga.com">Otesaga Hotel</a><br/>
+      Please note that there is a 3 night minimum stay
+    </li>
+    <li>
+      <a target="_blank" rel="noreferrer" href="https://www.ihg.com/holidayinnexpress/hotels/us/en/cooperstown/cophw/hoteldetail">Holiday Inn Express</a>
+      <br/>
+      Please note that there is a 2 night minimum stay
+    </li>
+  </ul>
+
+  <h2>Cooperstown Motels</h2>
+
+  <ul>
+    <li>
+      <a target="_blank" rel="noreferrer" href="https://www.cooperstownlakefronthotel.com/">Lakefront Hotel</a>
+    </li>
+    <li>
+      <a target="_blank" rel="noreferrer" href="https://lakeviewmotelny.com/">Lakeview Motel</a>
+    </li>
+  </ul>
+
+  <h2>Cooperstown Inns</h2>
+
+  <ul>
+    <li>
+      <a target="_blank" rel="noreferrer" href="https://www.landmarkinncooperstown.com/">Landmark Inn</a>
+    </li>
+    <li>
+      <a target="_blank" rel="noreferrer" href="https://www.innatcooperstown.com/">The Inn at Cooperstown</a>
+    </li>
+    <li>
+      <a target="_blank" rel="noreferrer" href="https://therailroadinn.com/">The Railroad Inn</a>
+    </li>
+    <li>
+      <a target="_blank" rel="noreferrer" href="http://baysidecooperstown.com/">Bayside Inn and Marina</a>
+    </li>
+    <li>
+      <a target="_blank" rel="noreferrer" href="https://cobblescote.com/">Cobblescotte on the Lake</a>
+    </li>
+  </ul>
+
+  <h2>Cooperstown Vacation Rentals</h2>
+
+  <ul>
+    <li>
+      <a target="_blank" rel="noreferrer" href="https://www.airbnb.com/s/Cooperstown--New-York--United-States">Airbnb</a>
+    </li>
+    <li>
+      <a target="_blank" rel="noreferrer" href="https://www.otsegolakelady.com/copy-of-list-all">Lake Lady</a>
+    </li>
+  </ul>
+</div>
