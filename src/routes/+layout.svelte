ebf200aa5d5dffe60c9c69286c2eb9583c6782b4
Dan Moseley <danomoseley@gmail.com>
2023-12-02 17:17:11 -0500

    More updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,18 +6,20 @@
 
   let open;
 </script>
-<div class="header">
-  <div class="left">
-    <Hamburger bind:open --color="rgb(30,87,134)" />
+<div id="container">
+  <div id="header">
+    <div class="left">
+      <Hamburger bind:open --color="rgb(30,87,134)" />
+    </div>
+    <div class="center">
+      Jenny & Dan
+    </div>
+    <div class="right">
+    </div>
   </div>
-  <div class="center">
-    Jenny & Dan
-  </div>
-  <div class="right">
-  </div>
-</div>
 
-<Menu bind:open />
-<div id="content">
-  <slot></slot>
+  <Menu bind:open />
+  <div id="content">
+    <slot></slot>
+  </div>
 </div>
