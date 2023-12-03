cb0a6a088bc4e1f27056f96d43a6afbe34a808d0
Dan Moseley <danomoseley@gmail.com>
2023-12-03 08:22:34 -0500

    Update welcome page RSVP spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,7 @@ import us from '$lib/assets/us.jpg';
     Jenny Stafford and Dan Moseley are getting married! August 17, 2024 in Cooperstown, NY. We hope that you can join us!
   </p>
   <img id="us" alt="Jenny and Dan engagement photo at Watervale beach" src={us} />
-  <br/>
-  <a href="/rsvp"><div class="button">RSVP</div></a>
+  <p>
+    <a href="/rsvp"><div class="button">RSVP</div></a>
+  </p>
 </div>
