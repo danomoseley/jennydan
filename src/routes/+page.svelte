59edca8f21857f3803414e666679de1a5557c9b0
Dan Moseley <danomoseley@gmail.com>
2023-12-03 08:20:00 -0500

    Update welcome page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,12 +9,9 @@ import us from '$lib/assets/us.jpg';
 <div id="home">
   <h2>Welcome</h2>
   <p>
-    Jenny Stafford and Dan Moseley are getting married! We hope that you can join us.
+    Jenny Stafford and Dan Moseley are getting married! August 17, 2024 in Cooperstown, NY. We hope that you can join us!
   </p>
   <img id="us" alt="Jenny and Dan engagement photo at Watervale beach" src={us} />
-  <h3>August 17, 2024</h3>
-  <hr/>
-  <h3>Cooperstown, NY</h3>
   <br/>
   <a href="/rsvp"><div class="button">RSVP</div></a>
 </div>
