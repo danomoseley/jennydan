7911768cb39fe238c679024815c151a7d9877fc2
Dan Moseley <danomoseley@gmail.com>
2024-06-01 11:31:47 -0400

    Registry page layout

--- src/routes/registry/+page.svelte
+++ src/routes/registry/+page.svelte
@@ -6,9 +6,11 @@
 <div id="registry">
   <h2>Registry</h2>
   <p>
-    Your presence at our wedding is genuinely the best present we could ask for. If you wish to contribute to the next phase of our lives together, we've thought of a few things that might help us along the way!
+    Your presence at our wedding is genuinely the best present we could ask for.
   </p>
   <p>
+    If you wish to contribute to the next phase of our lives together, we've thought of a few things that might help us along the way!
+    </br>/br>
     <a href="https://withjoy.com/jennydanregistry/registry?irclickid=112wNzxb4xyKWym3XkR7-w%3AvUkHX13yVWwcmwQ0&utm_medium=affiliate&utm_source=impactradius&utm_campaign=wedding&utm_content=Skimbit%20Ltd.&irgwc=1" target="_blank"><div class="button">View Our Registry</div></a>
   </p>
 </div>
