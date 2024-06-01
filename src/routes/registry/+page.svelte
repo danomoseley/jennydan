88e97ef34c5751b518839eda8145edcdfc3e5994
Dan Moseley <danomoseley@gmail.com>
2024-06-01 11:17:49 -0400

    Add registry page

---
+++ src/routes/registry/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+  <title>Registry - Jenny & Dan</title>
+  <meta name="description" content="Your presence at our wedding is genuinely the best present we could ask for. If you wish to contribute to the next phase of our lives together, we've thought of a few things that might help us along the way!" />
+</svelte:head>
+
+<div id="registry">
+  <h2>Registry</h2>
+  <p>
+    Your presence at our wedding is genuinely the best present we could ask for. If you wish to contribute to the next phase of our lives together, we've thought of a few things that might help us along the way!
+  </p>
+  <p>
+    <a href="https://withjoy.com/jennydanregistry/registry?irclickid=112wNzxb4xyKWym3XkR7-w%3AvUkHX13yVWwcmwQ0&utm_medium=affiliate&utm_source=impactradius&utm_campaign=wedding&utm_content=Skimbit%20Ltd.&irgwc=1" target="_blank"><div class="button">View Our Registry</div></a>
+  </p>
+</div>
