9246538bb5c78b1fafbc19c1069f657bf197bc5e
Dan Moseley <danomoseley@gmail.com>
2024-08-19 23:08:03 -0400

    Photos v2.5.39

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -54,7 +54,8 @@
 </svelte:head>
 
 <main class="max-w-5xl py-12 mx-auto space-y-12">
-  <Label class="pb-2" for="multiple_files">Share your photos of our wedding with us by uploading them below! When your photos have uploaded successfully they will display below. When you refresh the page the photos will disappear, but don't worry we will still have them! Feel free to upload as many as you'd like.</Label>
+  <p>Share your photos of our wedding with us by uploading them here!</p>
+  <p>When your photos have uploaded successfully they will display below. If you refresh the page the photos will disappear, but don't worry we will still have them! Feel free to upload as many as you'd like.</p>
   <Fileupload id="multiple_files" multiple bind:files on:change={handleFileUpload} />
   <div id="spinner" style="display:none; margin:10px;"><Spinner size={8} /></div>
   <div id="uploaded_images"></div>
