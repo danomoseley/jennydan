4e20d7780263e7b835449660811a468acb51ac61
Dan Moseley <danomoseley@gmail.com>
2024-08-20 02:00:02 -0400

    Photos v2.5.75

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -82,6 +82,7 @@
       path1.setAttribute('fill-rule', 'evenodd')
       path1.setAttribute('clip-rule', 'evenodd')
       path1.setAttribute('d', 'M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z');
+      svg.appendChild(path1)
 
       span.appendChild(img)
       span.appendChild(svg)
