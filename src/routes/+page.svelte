428c04ffbc04d106c811e26d9487d6c92e1916f9
Dan Moseley <danomoseley@gmail.com>
2024-08-28 10:36:06 -0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 <div id="home">
   <h2>Wedding Weekend</h2>
   <p>
-    Jenny Stafford and Dan Moseley are getting married! August 17, 2024 in Cooperstown, NY.
+    Jenny Stafford and Dan Moseley are getting married! August 17, 2024 in Cooperstown, NY. 
   </p>
   <img id="us" alt="Jenny and Dan in the neighborhood they lived in NYC." src={us} />
   <br/><br/>
